9e6683c1512d3c88b8a5c784dbe2520896b68dfa
Sebastian Sierra <s.sierra470@uniandes.edu.co>
2016-05-21 18:41:16 +0200

    "Merge with Tim"

--- elyko/public/js/student_notes/StudentNotesCtrl.js
+++ elyko/public/js/student_notes/StudentNotesCtrl.js
@@ -3,8 +3,10 @@ angular.module('elyko')
         '$scope',
         'notesPromise',
         '$state',
-        function ($scope, notesPromise, $state) {
+        '$stateParams',
+        function ($scope, notesPromise, $state, $stateParams) {
             $scope.student = notesPromise[0];
             $scope.state = $state;
+            $scope.semester = $stateParams.semester;
         }
     ]);
